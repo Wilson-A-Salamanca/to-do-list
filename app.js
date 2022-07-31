3d071e9946958e30e372696e80d3a9649e0bd539
Wilson-A-Salamanca <wilsonsalamanca346@gmail.com>
2022-07-31 13:13:22 -0500

    add css and some js

--- app.js
+++ app.js
@@ -6,18 +6,19 @@ const containerCompletedTask = document.getElementById("container__completed");
 let tasks = [];
 let counter = 0;
 
+//template to the task 
 class Task {
-    constructor(id , text){
+    constructor(id , text , state){
         this.id = id;
         this.text = text;
+        this.state = state;
     }
 }
-/*
-let newTask = new Task(counter++, "sacar perros");
-let newTask1 = new Task(counter++, "perros");
-tasks.unshift(newTask)
-*/
+
+// trap the text
 let task = "";
+let cont = 0;
+
 text.addEventListener("change", (e) => {
     task = e.target.value;
 });
@@ -26,17 +27,49 @@ text.addEventListener("change", (e) => {
 //add task to the ul with a li tag 
 addText.addEventListener("click", (e) => {
     //HTML
-    const text = document.createElement("li");
-    
+    if(task !== ""){
+        let containerTask = document.createElement("li");
+        let taskContent ="";
+        taskContent += `     
+            <input type="checkbox" >
+            <button>delete</button>`;
 
-    text.innerHTML= task;
-    container.appendChild(text)
-    console.log(text)
 
-    //JS
-    let newTask = new Task(counter++, task)
-    tasks.unshift(newTask)
-
-    console.log(tasks)
+        containerTask.setAttribute("id", cont++)
+        containerTask.innerHTML = task + taskContent;
+        container.appendChild(containerTask);
+        
+        //JS
+        let newTask = new Task(counter++, task , false)
+        tasks.push(newTask)
+        //unshift this method if to add element in the array, but in different order
+        console.log(tasks)
+    }
 });
+
+//delete task of the ul 
+
+
+
+container.addEventListener('click', (e) => {
+    //console.log(e.target.style.visibility)
+   if(e.target.nodeName === "BUTTON"){
+        document.getElementById(e.target.parentNode.id).remove();
+        
+        const filterId = tasks.map((item)=>{ //filter
+            return item.id 
+        });
+
+        console.log(filterId)
+       /*
+        //if(tasks === e.target.parentNode.id)
+        tasks.splice(e.target.parentNode.id, 1);
+        console.log(tasks);
+        console.log(e.target.parentNode.id) */
+    }
+    //for(let i = 0 ; i <= tasks.length; i++){
+      //  console.log(tasks[i])
+    //}
+})
+
 
